e36d7c3215c12fa4b61c44fb70ef75ec8596dda1
rakeshpatel87p <rakesh.patel@accesscfa.com>
2020-06-01 20:12:01 -0500

    infra: convert to ts file

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -2,27 +2,32 @@ import React from "react";
 import { Link } from "gatsby";
 import navLinks from "./content.json";
 
-import HeaderStyle from "./styles/header";
+import HeaderStyle from "./styles";
+import { css } from "styled-components";
 
 interface Props {
   siteTitle: string;
+  isSticky: boolean;
+  element: HTMLElement;
 }
 
-const Header = ({ siteTitle }: Props): JSX.Element => (
-  <HeaderStyle>
-    <h1>
-      <Link>{siteTitle}</Link>
-    </h1>
-    <nav>
-      <ul>
-        {navLinks.map((link, i) => (
-          <li key={`${link.name}_${i}`}>
-            <Link to={link.link}>{link.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  </HeaderStyle>
-);
+const Header = (props: Props): JSX.Element => {
+  return (
+    <HeaderStyle ref={props.element} isSticky={props.isSticky}>
+      <h1>
+        <Link to={"/"}>{props.siteTitle}</Link>
+      </h1>
+      <nav>
+        <ul>
+          {navLinks.map((link, i) => (
+            <li key={`${link.name}_${i}`}>
+              <Link to={link.link}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </HeaderStyle>
+  );
+};
 
 export default Header;
